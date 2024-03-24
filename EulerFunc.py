520a47a9a8719f6d746d32600623927e4352b814
Gargamel-l <belyton2002@yandex.ru>
2024-03-24 20:15:17 +0300

    Add project files.

---
+++ EulerFunc.py
@@ -0,0 +1,7 @@
+﻿from sympy import totient
+
+# Вычисляем значение функции Эйлера (функции тотиента) для n=35
+n = 35
+phi_n = totient(n)
+phi_n
+
---
+++ InverseValue.py
@@ -0,0 +1,12 @@
+﻿
+from sympy import mod_inverse
+
+# Значения a и m из условия
+a = 5
+m = 7
+
+# Находим обратное по модулю для a
+a_inv_mod_m = mod_inverse(a, m)
+a_inv_mod_m
+
+
